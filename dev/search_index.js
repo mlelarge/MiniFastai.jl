d5c0a7e41aeb27684021897777df8a7c7423e914
Documenter.jl <documenter@juliadocs.github.io>
2020-08-31 21:06:27 +0000

    build based on 276bcdd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MiniFastai","category":"page"},{"location":"#MiniFastai","page":"Home","title":"MiniFastai","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MiniFastai]","category":"page"},{"location":"#MiniFastai.my_f-Tuple{Any,Any}","page":"Home","title":"MiniFastai.my_f","text":"my_f(x,y)\n\nA linear function.\n\nExamples\n\njulia> my_f(1,2) 8\n\n\n\n\n\n","category":"method"}]
+}
